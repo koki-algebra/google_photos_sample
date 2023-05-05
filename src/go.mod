e75d83453252fc496f6664632806fa0ed4cfd591
KokiYamada <nino02000@icloud.com>
2023-05-05 05:50:35 +0000

    add chi

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module github.com/koki-algebra/google_photos_sample
 go 1.20
 
 require (
+	github.com/go-chi/chi/v5 v5.0.8
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/oauth2 v0.7.0
 	golang.org/x/sync v0.2.0
--- src/mux.go
+++ src/mux.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"golang.org/x/oauth2"
 )
 
 func NewServMux(config *oauth2.Config) (http.Handler, func(), error) {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello, Google Photos Library!")
 	})
-	
+
 	return mux, func() {}, nil
 }
