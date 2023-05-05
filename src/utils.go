4c75120865f13c78dc0320dd9c17e69841797684
KokiYamada <nino02000@icloud.com>
2023-05-05 07:26:51 +0000

    add get token from local

--- src/utils.go
+++ src/utils.go
@@ -4,9 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 
 	"golang.org/x/oauth2"
 )
+
+type TokenSource struct {
+	TokenType    string    `json:"token_type"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	Expiry       time.Time `json:"expiry"`
+}
 
 func SaveToken(filepath string, token *oauth2.Token) (err error) {
 	file, err := os.Create(filepath)
@@ -16,15 +24,14 @@ func SaveToken(filepath string, token *oauth2.Token) (err error) {
 		}
 	}()
 	if err != nil {
-		return err
+		return
 	}
 
-	data := struct {
-		AccessToken  string `json:"access_token"`
-		RefreshToken string `json:"refresh_token"`
-	}{
+	data := TokenSource{
+		TokenType:    token.TokenType,
 		AccessToken:  token.AccessToken,
 		RefreshToken: token.RefreshToken,
+		Expiry:       token.Expiry,
 	}
 
 	if err = json.NewEncoder(file).Encode(&data); err != nil {
@@ -32,4 +39,30 @@ func SaveToken(filepath string, token *oauth2.Token) (err error) {
 	}
 
 	return nil
+}
+
+func GetTokenFromLocal(filepath string) (token *oauth2.Token, err error) {
+	file, err := os.Open(filepath)
+	defer func() {
+		if closeErr := file.Close(); closeErr != nil {
+			err = fmt.Errorf("defer close error: %v", closeErr)
+		}
+	}()
+	if err != nil {
+		return
+	}
+
+	var data TokenSource
+	if err = json.NewDecoder(file).Decode(&data); err != nil {
+		return
+	}
+
+	token = &oauth2.Token{
+		AccessToken:  data.AccessToken,
+		TokenType:    data.TokenType,
+		RefreshToken: data.RefreshToken,
+		Expiry:       data.Expiry,
+	}
+
+	return
 }
